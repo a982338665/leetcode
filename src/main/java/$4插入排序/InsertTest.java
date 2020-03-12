7216299df7df98bd81d139ad6a90d9e81793bcef
a982338665 <982338665@qq.com>
2020-03-12 11:16:42 +0800

    插入排序-逆序

--- src/main/java/$4插入排序/InsertTest.java
+++ src/main/java/$4插入排序/InsertTest.java
@@ -9,20 +9,23 @@ public class InsertTest {
         System.err.println(Arrays.toString(desc));
         int[] desc2 = insertionSort(new int[]{1, 23, 4, 5, 6, 7777, 87, 7777});
         System.err.println(Arrays.toString(desc2));
+        int[] desc3 = descSort(new int[]{1, 23, 4, 5, 6, 7777, 87, 7777});
+        System.err.println(Arrays.toString(desc3));
     }
 
     /**
      * 算比较-选择排序
+     *
      * @param x
      * @return
      */
     public static int[] desc(int[] x) {
         for (int i = 1; i < x.length; i++) {
-            for (int j = 0; j < i ; j++) {
+            for (int j = 0; j < i; j++) {
                 //第0个位置为x[j] 第一个位置为x[i]
                 //依次比较判断，总是把大的插在最前面,并假设前面的数据已经排好序
                 //则通过比较，将值插入
-                if (x[j]<x[i]){//第一个位置大于第二个位置，则交换，为逆序
+                if (x[j] < x[i]) {//第一个位置大于第二个位置，则交换，为逆序
                     //交换位置
                     int temp = x[j];
                     x[j] = x[i];
@@ -36,6 +39,7 @@ public class InsertTest {
 
     /**
      * 插入排序
+     *
      * @param array
      * @return
      */
@@ -57,4 +61,33 @@ public class InsertTest {
         }
         return array;
     }
+
+    /**
+     * 插入排序
+     *
+     * @param array
+     * @return
+     */
+    public static int[] descSort(int[] array) {
+        if (array.length < 2)
+            return array;
+        for (int i = 0; i < array.length - 1; i++) {
+            //定义当前值,从下标为1开始，左边为已排序，右边为待排序
+            int curr = array[i + 1];//23
+            //定义前一个值的索引
+            int pre = i;//0
+            //开始循环左边数组,从第一个开始，故为i+1
+            //如果当前的 大于 前一个
+            //必须是大于等于0,否则第一第二个将会被跳过
+            for (; pre >= 0 && curr < array[pre]; pre--) {
+                //前一个后移位置，为插入留好空档
+                array[pre + 1] = array[pre];
+            }
+            //留好得空档插入当前值
+            array[pre + 1] = curr;
+        }
+        return array;
+    }
+
+
 }
